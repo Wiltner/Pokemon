fc075696425af976c0edeffb60a33cfe7fc777f4
MatheusWiltner <89705762+Wiltner@users.noreply.github.com>
2024-06-11 21:28:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,161 @@
+import React from 'react';
+
+
+
+const Pokemon1 = () =>{
+  return(
+    <div>
+      <h1>Bulbassauro</h1>
+      <p>Tipo: Planta & Venenoso</p>
+      <p>HP: 45</p>
+      <p>Ataque: 49</p>
+      <p>Defesa: 49</p>
+      <p>Velocidade: 45</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon2 = () =>{
+  return(
+    <div>
+      <h1>Ivysaur</h1>
+      <p>Tipo: Planta & Venenoso</p>
+      <p>HP: 60</p>
+      <p>Ataque: 62</p>
+      <p>Defesa: 63</p>
+      <p>Velocidade: 60</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon3 = () =>{
+  return(
+    <div>
+      <h1>Venusaur</h1>
+      <p>Tipo: Planta & Venenoso</p>
+      <p>HP: 80</p>
+      <p>Ataque: 82</p>
+      <p>Defesa: 83</p>
+      <p>Velocidade: 80</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon4 = () =>{
+  return(
+    <div>
+      <h1>Mega Venusaur</h1>
+      <p>Tipo: Planta & Venenoso</p>
+      <p>HP: 80</p>
+      <p>Ataque: 100</p>
+      <p>Defesa: 123</p>
+      <p>Velocidade: 80</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon5 = () =>{
+  return(
+    <div>
+      <h1>Charmander</h1>
+      <p>Tipo: Fogo</p>
+      <p>HP: 39</p>
+      <p>Ataque: 52</p>
+      <p>Defesa: 43</p>
+      <p>Velocidade: 65</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon6 = () =>{
+  return(
+    <div>
+      <h1>Charmeleon</h1>
+      <p>Tipo: Fogo</p>
+      <p>HP: 58</p>
+      <p>Ataque: 64</p>
+      <p>Defesa: 58</p>
+      <p>Velocidade: 80</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon7 = () =>{
+  return(
+    <div>
+      <h1>Charizard</h1>
+      <p>Tipo: Fogo & Voador</p>
+      <p>HP: 78</p>
+      <p>Ataque: 84</p>
+      <p>Defesa: 78</p>
+      <p>Velocidade: 100</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon8 = () =>{
+  return(
+    <div>
+      <h1>Squirtle</h1>
+      <p>Tipo: Água</p>
+      <p>HP: 44</p>
+      <p>Ataque: 48</p>
+      <p>Defesa: 65</p>
+      <p>Velocidade: 43</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon9 = () =>{
+  return(
+    <div>
+      <h1>Wartortle</h1>
+      <p>Tipo: Água</p>
+      <p>HP: 59</p>
+      <p>Ataque: 63</p>
+      <p>Defesa: 80</p>
+      <p>Velocidade: 58</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+const Pokemon10 = () =>{
+  return(
+    <div>
+      <h1>Blastoise</h1>
+      <p>Tipo: Água</p>
+      <p>HP: 79</p>
+      <p>Ataque: 83</p>
+      <p>Defesa: 100</p>
+      <p>Velocidade: 78</p>
+      <p>Geração: 1</p>
+    </div>
+  )
+}
+
+function Pokemon(){
+  return(
+    <div>
+      <Pokemon1/>
+      <Pokemon2/>
+      <Pokemon3/>
+      <Pokemon4/>
+      <Pokemon5/>
+      <Pokemon6/>
+      <Pokemon7/>
+      <Pokemon8/>
+      <Pokemon9/>
+      <Pokemon10/>
+    </div>
+  )
+}
+export default Pokemon;
